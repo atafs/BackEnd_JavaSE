dd27d8010ae107d3c77efcf60193aedf14fa678a
atafs <atafs@iscte-iul.pt>
2016-01-17 19:45:38 +0000

    update

---
+++ ProjectoJava_Modulos_IO_v2/src/parser/jdom2/GenerateXMLDriver.java
@@ -0,0 +1,16 @@
+package parser.jdom2;
+
+public class GenerateXMLDriver {
+	
+	//ATTRIBUTES
+	
+	//MAIN
+	public static void main(String[] args) {
+		
+	}
+	
+	/** STATIC FUNCTION: */
+	
+	/** STATIC PROCEDURE: */
+
+}
